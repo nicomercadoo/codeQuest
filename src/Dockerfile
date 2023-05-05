f001e6429f037b78435c98ed6e556b08ad0cec35
Nicolas Mercado <nicolasmercado452@gmail.com>
2023-05-05 20:27:53 -0300

    Se prepara el entorno de trabajo
    Se prepara el entorno de trabajo para empezar a desarrollar

---
+++ src/Dockerfile
@@ -0,0 +1,10 @@
+From ruby:2.7.5
+
+ADD . /src
+WORKDIR /src
+
+RUN bundle install
+
+EXPOSE 4567
+
+CMD ["/bin/bash"]
---
+++ src/server.rb
@@ -0,0 +1,43 @@
+require 'sinatra/base'
+require 'bundler/setup'
+require 'logger'
+require 'sinatra/activerecord'
+
+require 'sinatra/reloader' if Sinatra::Base.environment == :development
+
+
+require_relative 'models/user'
+
+class App < Sinatra::Application
+  def initialize(app = nil)
+    super()
+  end
+
+  configure :production, :development do
+    enable :logging
+
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG if development?
+    set :logger, logger
+  end
+
+
+  configure :development do
+    register Sinatra::Reloader
+    after_reload do
+      puts 'Reloaded...'
+    end
+  end
+
+  get '/game' do
+    logger.info 'USANDO LOGGER INFO EN GAME PATH'
+    'Game'
+  end
+
+
+
+  get '/' do
+    'Welcome'
+  end
+end
+
