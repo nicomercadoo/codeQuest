8b090386846ccc7337dd666e5fa09d56665f4e90
Nicolas Mercado <nicolasmercado452@gmail.com>
2023-06-16 00:29:30 -0300

    Tests de Account

--- src/models/account.rb
+++ src/models/account.rb
@@ -5,9 +5,10 @@ class Account < ActiveRecord::Base
   validates :nickname, presence: true
   validates :progress, presence: true
   validates :theme_light, inclusion: { in: [true, false] }
+  validates :correct_format_of_fields?
 
   has_many :account_lessons
-  has_many :lessons, through: :account_lessons 
+  has_many :lessons, through: :account_lessons
   has_many :account_options
   has_many :options, through: :account_options
   has_many :account_questions
@@ -16,29 +17,37 @@ class Account < ActiveRecord::Base
   has_many :tests, through: :account_tests
 
 
-  after_commit :actualizar_progreso
+  after_commit :update_progress
 
+  def correct_format_of_fields?
+    valid_email_format = /^[a-zA-Z0-9_.+-]+@(gmail|outlook|hotmail|live)\.[a-z.]+$/
+    valid_password_format = /(?=(?:.*[A-Z].*)+)(?=(?:.*[a-z].*)+)(?=(?:.*\d.*)+)(?!(?:.*\s.*)+)^(?=.{8,}$).*/
+    valid_name_format = /(?=(?:^\D*$)+)/
+    valid_nickname_format = /(?=(?:^\S*$)+)/
 
-  def actualizar_progreso
+    valid_email_format.match(email) && valid_password_format.match(password) && valid_name_format.match(name) && valid_nickname_format.match(nickname)
+  end
+
+  def update_progress
     # Lógica para actualizar el progreso de la cuenta
-  
+
     # Los capítulos valen el 100% del progreso (1 cap 100%, 2 caps 50%, etc)
     total_chapters = Test.distinct.count(:letter)
     total_progress = 100
     chapter_progress = total_chapters != 0 ? total_progress / total_chapters : 0
-  
+
     # El test vale el 50% del progreso de un capítulo
     total_tests = Test.distinct.count(:letter)
     completed_tests = account_tests.where(test_completed: true).count
     test_progress = total_tests != 0 ? chapter_progress / 2 : 0
     completed_tests_progress = completed_tests * test_progress
-  
+
     # Las lecciones valen el otro 50% del progreso de un capítulo
     total_lessons = Lesson.distinct.count(:test_letter)
     completed_lessons = account_lessons.where(lesson_completed: true).count
     lesson_progress = total_lessons != 0 ? chapter_progress / (2 * total_lessons) : 0
     completed_lessons_progress = completed_lessons * lesson_progress
-  
+
     # Calcular el progreso total
     new_progress = completed_tests_progress + completed_lessons_progress
     update_column(:progress, new_progress)
--- src/spec/models/account_spec.rb
+++ src/spec/models/account_spec.rb
@@ -1,12 +1,65 @@
 require_relative '../../models/init'
+require_relative '../../models/account'
+require_relative '../../models/lesson'
+require_relative '../../models/test'
+require_relative '../../models/option'
+require_relative '../../models/question'
+require_relative '../../models/account_lesson'
+require_relative '../../models/account_test'
+require_relative '../../models/account_option'
+require_relative '../../models/account_question'
 
-describe 'Account' do
-  describe 'valid' do
-    describe 'when there is no name' do
-      it 'should be invalid' do
-        account = Account.new
-        expect(account.valid?).to eq(false)
+describe Account do
+  describe 'Valid' do
+    context 'valid create situations' do
+      it 'should be valid' do
+        # Arrange
+        account = Account.new(name: 'Juan', email: 'juanito@gmail.com', password: 'Juanito32', nickname: 'juanito', theme_light: 'dark')
+        # Assert
+        expect(account.valid?).to be_truthy
+      end
+    end
+    context 'not valid situations' do
+      it 'should be invalid (email blame)' do
+        # Arrange
+        account = Account.new(name: 'Juan', email: 'juanito@yieil.com', password: 'Juanito32', nickname: 'juanito', theme_light: 'dark')
+        # Assert
+        expect(account.valid?).to be_falsey
+      end
+
+      it 'should be invalid (password blame)' do
+        # Arrange
+        account = Account.new(name: 'Juan', email: 'juanito@gmail.com', password: 'juanito3', nickname: 'juanito', theme_light: 'dark')
+        # Assert
+        expect(account.valid?).to be_falsey
+      end
+
+      it 'should be invalid (nickname blame)' do
+        # Arrange
+        account = Account.new(name: 'Juan', email: 'juanito@gmail.com', password: 'Juanito32', nickname: 'ju anito', theme_light: 'dark')
+        # Assert
+        expect(account.valid?).to be_falsey
+      end
+
+      it 'should be invalid (name blame)' do
+        # Arrange
+        account = Account.new(name: 'Juanit0', email: 'juanito@gmail.com', password: 'Juanito32', nickname: 'juanito', theme_light: 'dark')
+        # Assert
+        expect(account.valid?).to be_falsey
       end
     end
   end
+  describe 'Create same account' do
+    it 'should be invalid (email blame)' do
+      # Arrange
+      account1 = Account.new(name: 'Juan', email: 'juanito@gmail.com', password: 'Juanito32', nickname: 'juanito', theme_light: 'dark')
+      # Act
+      account1.save
+      account2 = Account.new(name: account1.name, email: account1.email, password: account1.password, nickname: account1.nickname, theme_light: account1.theme_light)
+      # Assert
+      expect(account2.save).to be_truthy
+    end
+  end
 end
+
+
