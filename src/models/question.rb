8dc59f26b887895439ade09b9dfd36c5cb8524ed
Nicolas Mercado <nicolasmercado452@gmail.com>
2023-10-16 01:04:18 -0300

    [#186236346] Se agrega la logica de renderizado para las preguntas

--- src/models/question.rb
+++ src/models/question.rb
@@ -8,4 +8,16 @@ class Question < ActiveRecord::Base
   has_many :account_options
   has_many :accounts, through: :account_questions
   has_many :account_questions
+
+  # Retorna el contenido de una pregunta.
+  def content
+    # Se lee el archivo de la lección
+    question_file_path = File.join("/src/tests", "#{test_letter}/Q-#{test_letter}-#{number}/Q-#{number}.adoc")
+    question_file_content = File.read question_file_path, mode: 'r:utf-8'
+
+    # Se renderiza el contenido
+    stylesheet_path = "/src/stylesheets/test.css"
+    Asciidoctor.convert question_file_content, safe: :safe, attributes: { 'showtitle' => true,'stylesheet' => stylesheet_path }
+  end
+
 end
